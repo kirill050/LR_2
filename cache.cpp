041185c61cb0356c8886ae3593f43dac049cd104
kirill050 <kirilltikhonov050@gmail.com>
2019-12-06 15:34:12 +0300

    06.12.19 15:34

--- cache.cpp
+++ cache.cpp
@@ -19,6 +19,7 @@ public:
 		cache_sizes['2'] = l2;
 		cache_sizes['3'] = l3;
 	}
+
 	Experiment(Experiment &prev_ex){
 		if (prev_ex.travel_variant == string("Direct")){
 			travel_variant = "Return";
@@ -28,16 +29,70 @@ public:
 			_ofile.open("random.txt");
 			travel_variant = "Random";
 		}
-		//cache_sizes = prev_ex.cache_sizes;
 		cache_sizes['1'] = prev_ex.cache_sizes['1'];
-                cache_sizes['2'] = prev_ex.cache_sizes['2'];
-                cache_sizes['3'] = prev_ex.cache_sizes['3'];
+        cache_sizes['2'] = prev_ex.cache_sizes['2'];
+        cache_sizes['3'] = prev_ex.cache_sizes['3'];
 	}
+
 	~Experiment(){
 		_ofile.close();
 		delete _buffer;
 	}
+
 	string get_travel_variant(){return travel_variant;}
+
+	void count_number_of_experiments(){
+		int buf_size =  cache_sizes['1'] * 0.5;
+		_buffer_sizes.push_back(buf_size);
+		float osnovaniye = 0;
+		if ( (cache_sizes['3']) ){
+			int max_buf = 1.5 * (cache_sizes['3']);
+                        while (pow(2.0, osnovaniye) < max_buf ){
+                                buf_size = pow(2.0, osnovaniye);
+                                if (buf_size <= _buffer_sizes[0]){
+					if ( (cache_sizes['1']) < (cache_sizes['2']) ){
+                                        	osnovaniye++;
+                                        	continue;
+					}
+                                }
+                                _buffer_sizes.push_back(buf_size);
+                                osnovaniye++;
+                        }
+                        _buffer_sizes.push_back(max_buf);
+		}
+		else{
+			int max_buf = 1.5 * (cache_sizes['2']);
+			while (pow(2.0, osnovaniye) < max_buf ){
+				buf_size = pow(2.0, osnovaniye);
+				if (buf_size == _buffer_sizes[0]){
+					if ( (cache_sizes['1']) < (cache_sizes['2']) ){
+                                                osnovaniye++;
+                                                continue;
+                                        }
+				}
+				_buffer_sizes.push_back(buf_size);
+				osnovaniye++;
+			}
+			_buffer_sizes.push_back(max_buf);
+		}
+		_number_of_experiments = _buffer_sizes.size();
+	}
+
+	void create_test_buffer(const int _quantity){
+		_buffer = new unsigned char[_quantity];
+		for (int i = 0; i < _quantity; i++){
+			_buffer[i] = rand()%256;
+		}
+	}
+
+	void warm_up_cache(const int _size){
+		for (int j = 0; j < 100; j++){
+			for (int k = 0; k < _size; k++){
+				_buffer[k] = rand()%255;
+			}
+		}
+	}
+
 	void run(int _size){
 		_ofile << _size << "  ";
 		unsigned start = 0;
@@ -76,6 +131,7 @@ public:
 
 		stop(start);
 	}
+
 	void stop(unsigned start){
 		unsigned stop_t = clock();
 		unsigned duration = stop_t - start;
@@ -83,55 +139,7 @@ public:
 		_duration.push_back(duration);
 		_ofile << ((float) duration) / CLOCKS_PER_SEC << endl;
 	}
-	void count_number_of_experiments(){
-		int buf_size =  cache_sizes['1'] * 0.5;
-		_buffer_sizes.push_back(buf_size);
-		float osnovaniye = 0;
-		if ( (cache_sizes['3']) ){
-			int max_buf = 1.5 * (cache_sizes['3']);
-                        while (pow(2.0, osnovaniye) < max_buf ){
-                                buf_size = pow(2.0, osnovaniye);
-                                if (buf_size <= _buffer_sizes[0]){
-					if ( (cache_sizes['1']) < (cache_sizes['2']) ){
-                                        	osnovaniye++;
-                                        	continue;
-					}
-                                }
-                                _buffer_sizes.push_back(buf_size);
-                                osnovaniye++;
-                        }
-                        _buffer_sizes.push_back(max_buf);
-		}
-		else{
-			int max_buf = 1.5 * (cache_sizes['2']);
-			while (pow(2.0, osnovaniye) < max_buf ){
-				buf_size = pow(2.0, osnovaniye);
-				if (buf_size == _buffer_sizes[0]){
-					if ( (cache_sizes['1']) < (cache_sizes['2']) ){
-                                                osnovaniye++;
-                                                continue;
-                                        }
-				}
-				_buffer_sizes.push_back(buf_size);
-				osnovaniye++;
-			}
-			_buffer_sizes.push_back(max_buf);
-		}
-		_number_of_experiments = _buffer_sizes.size();
-	}
-	void create_test_buffer(const int _quantity){
-		_buffer = new unsigned char[_quantity];
-		for (int i = 0; i < _quantity; i++){
-			_buffer[i] = rand()%256;
-		}
-	}
-	void warm_up_cache(const int _size){
-		for (int j = 0; j < 100; j++){
-			for (int k = 0; k < _size; k++){
-				_buffer[k] = rand()%255;
-			}
-		}
-	}
+
 	void print_results(){
 		cout << "investigation:" << endl;
 		cout << "\ttravel_variant: " << travel_variant << endl;
@@ -150,6 +158,7 @@ public:
 			cout << endl;
 		}
 	}
+
 	void just_do_it(){
 		count_number_of_experiments();
 		for (int j = 0; j < _number_of_experiments; j++){
@@ -164,16 +173,20 @@ public:
 		}
 		print_results();
 	}
-public:// DO NOT FORGET TO MAKE THEM PRIVATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+public:
 	string travel_variant;
 	map <char, int> cache_sizes;
+
+private:
 	int _number_of_experiments;
 	vector <int> _buffer_sizes;
 	unsigned char *_buffer;
 	vector <int> _duration;
 	ofstream _ofile;
 };
-void pushnoy(int l1, int l2, int l3){
+
+void Pushnoy(int l1, int l2, int l3){
 	Experiment ex_1(l1, l2, l3);
 	ex_1.just_do_it();
 
@@ -181,10 +194,10 @@ void pushnoy(int l1, int l2, int l3){
 	ex_2.just_do_it();
 
 	Experiment ex_3 = ex_2;
-        ex_3.just_do_it();
+    ex_3.just_do_it();
 }
 int main(){
 	cout << "It`s alive!" << endl;
-        pushnoy(32, 3, 0);
+    Pushnoy(32, 3, 0);
 	return 0;
 }
