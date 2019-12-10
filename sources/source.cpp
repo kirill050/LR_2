77b845e0bc8e6a26c9e9435bae4b663062bdabc2
kirill050 <kirilltikhonov050@gmail.com>
2019-12-10 22:12:16 +0300

    10.12.19 22:12

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,4 +1,4 @@
-//Copyright 2018 Your Name <your_email>"
+// Copyright 2018 Your Name <your_email>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,11 +9,16 @@
 #include <algorithm>
 #include <fstream>
 
-using namespace std;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
 
-class Experiment{
+class Experiment
+{
 public:
-    Experiment(const int &l1, const int &l2, const int &l3){
+    Experiment(const int &l1, const int &l2, const int &l3)
+    {
         travel_variant = "Direct";
         _ofile.open("direct.txt");
         cache_sizes['1'] = l1;
@@ -21,11 +26,13 @@ public:
         cache_sizes['3'] = l3;
     }
 
-    Experiment(Experiment &prev_ex){
-        if (prev_ex.travel_variant == string("Direct")){
+    Experiment(Experiment &prev_ex)
+    {
+        if (prev_ex.travel_variant == string("Direct"))
+        {
             travel_variant = "Return";
             _ofile.open("return.txt");
-        }else{
+        } else {
             _ofile.open("random.txt");
             travel_variant = "Random";
         }
@@ -34,23 +41,32 @@ public:
         cache_sizes['3'] = prev_ex.cache_sizes['3'];
     }
 
-    ~Experiment(){
+    ~Experiment()
+    {
         _ofile.close();
         delete _buffer;
     }
 
-    string get_travel_variant(){return travel_variant;}
+    string get_travel_variant()
+    {
+        return travel_variant;
+    }
 
-    void count_number_of_experiments(){
+    void count_number_of_experiments()
+    {
         int buf_size =  cache_sizes['1'] * 0.5;
         _buffer_sizes.push_back(buf_size);
         float osnovaniye = 0;
-        if ( (cache_sizes['3']) ){
+        if ((cache_sizes['3']))
+        {
             int max_buf = 1.5 * (cache_sizes['3']);
-                        while (pow(2.0, osnovaniye) < max_buf ){
+                        while (pow(2.0, osnovaniye) < max_buf)
+                        {
                                 buf_size = pow(2.0, osnovaniye);
-                                if (buf_size <= _buffer_sizes[0]){
-                    if ( (cache_sizes['1']) < (cache_sizes['2']) ){
+                                if (buf_size <= _buffer_sizes[0])
+                                {
+                    if ((cache_sizes['1']) < (cache_sizes['2']))
+                    {
                                             osnovaniye++;
                                             continue;
                     }
@@ -59,12 +75,15 @@ public:
                                 osnovaniye++;
                         }
                         _buffer_sizes.push_back(max_buf);
-        }else{
+        } else {
             int max_buf = 1.5 * (cache_sizes['2']);
-            while (pow(2.0, osnovaniye) < max_buf ){
+            while (pow(2.0, osnovaniye) < max_buf)
+            {
                 buf_size = pow(2.0, osnovaniye);
-                if (buf_size == _buffer_sizes[0]){
-                    if ( (cache_sizes['1']) < (cache_sizes['2']) ){
+                if (buf_size == _buffer_sizes[0])
+                {
+                    if ((cache_sizes['1']) < (cache_sizes['2']))
+                    {
                                                 osnovaniye++;
                                                 continue;
                                         }
@@ -77,7 +96,8 @@ public:
         _number_of_experiments = _buffer_sizes.size();
     }
 
-    void create_test_buffer(const int _quantity){
+    void create_test_buffer(const int _quantity)
+    {
         _buffer = new unsigned char[_quantity];
         for (int i = 0; i < _quantity; i++){
             _buffer[i] = rand()%256;
@@ -92,44 +112,54 @@ public:
         }
     }
 
-    void run(int _size){
+    void run(int _size)
+    {
         _ofile << _size << "  ";
         unsigned start = 0;
-        if (travel_variant == "Direct"){
+        if (travel_variant == "Direct")
+        {
             start = clock();
-            for (int ii = 0; ii < 1000; ii++){
+            for (int ii = 0; ii < 1000; ii++)
+            {
                 srand(time(NULL));
-                for (int jj = 0; jj < _size; jj++){
+                for (int jj = 0; jj < _size; jj++)
+                {
                     _buffer[jj] = rand()%255;
                 }
             }
-        }else if (travel_variant == "Return"){
+        } else if (travel_variant == "Return") {
             start = clock();
-            for (int ii = 0; ii < 1000; ii++){
-                                srand(time(NULL));
-                                for (int jj = (_size -1); jj >= 0 ; jj--){
-                                        _buffer[jj] = rand()%255;
-                                }
-                        }
-        }else{
+            for (int ii = 0; ii < 1000; ii++)
+            {
+                srand(time(NULL));
+                for (int jj = (_size -1); jj >= 0 ; jj--)
+                {
+                    _buffer[jj] = rand()%255;
+                }
+             }
+        } else {
             vector <int> random_indexes;
-            for (int cx = 0; cx < _size; cx++){
+            for (int cx = 0; cx < _size; cx++)
+            {
                 random_indexes.push_back(cx);
             }
             random_shuffle(random_indexes.begin(), random_indexes.end());
             start = clock();
-            for (int ii = 0; ii < 1000; ii++){
-                                srand(time(NULL));
-                                for (int jj = 0; jj < _size; jj++){
-                                        _buffer[random_indexes[jj]] = rand()%255;
-                                }
-                        }
+            for (int ii = 0; ii < 1000; ii++)
+            {
+                 srand(time(NULL));
+                 for (int jj = 0; jj < _size; jj++)
+                 {
+                     _buffer[random_indexes[jj]] = rand()%255;
+                  }
+             }
         }
 
         stop(start);
     }
 
-    void stop(unsigned start){
+    void stop(unsigned start)
+    {
         unsigned stop_t = clock();
         unsigned duration = stop_t - start;
         cout << ((float) duration) / CLOCKS_PER_SEC << "sec" << endl;
@@ -137,29 +167,36 @@ public:
         _ofile << ((float) duration) / CLOCKS_PER_SEC << endl;
     }
 
-    void print_results(){
+    void print_results()
+    {
         cout << "investigation:" << endl;
         cout << "\ttravel_variant: " << travel_variant << endl;
         cout << "\texperiments:" << endl;
-        for (int i = 0; i < _number_of_experiments; i++){
+        for (int i = 0; i < _number_of_experiments; i++)
+        {
             cout << "\t--experiment:" << endl;
             cout << "\t\tnumber:" << i << endl;
             cout << "\t\tinput data: "  << endl;
             cout << "\t\t buffer_size:";
-            if ( (_buffer_sizes[0] >= _buffer_sizes[1]) && (!i)){
+            if ((_buffer_sizes[0] >= _buffer_sizes[1]) && (!i))
+            {
                             cout << _buffer_sizes[i] << "kb ";
-                    }else cout << _buffer_sizes[i] << "mb ";
+                    } else cout << _buffer_sizes[i] << "mb ";
             cout << endl << "\t\tresults:" << endl;
-            cout << "\t\t duration: " << ((float) _duration[i]) / CLOCKS_PER_SEC << "sec";
+            cout << "\t\t duration: ";
+            cout << ((float) _duration[i]) / CLOCKS_PER_SEC << "sec";
             cout << endl;
         }
     }
 
-    void just_do_it(){
+    void just_do_it()
+    {
         count_number_of_experiments();
-        for (int j = 0; j < _number_of_experiments; j++){
+        for (int j = 0; j < _number_of_experiments; j++)
+        {
             int factor = pow(2.0, 20.0);
-            if ( (_buffer_sizes[0] >= _buffer_sizes[1]) && (!j) ){
+            if ((_buffer_sizes[0] >= _buffer_sizes[1]) && (!j))
+            {
                  factor = pow(2.0, 10.0);
             }
             int size = _buffer_sizes[j]*factor;
@@ -182,7 +219,8 @@ private:
     ofstream _ofile;
 };
 
-void Pushnoy(int l1, int l2, int l3){
+void Pushnoy(int l1, int l2, int l3)
+{
     Experiment ex_1(l1, l2, l3);
     ex_1.just_do_it();
 
@@ -192,7 +230,8 @@ void Pushnoy(int l1, int l2, int l3){
     Experiment ex_3 = ex_2;
     ex_3.just_do_it();
 }
-int main(){
+int main()
+{
     cout << "It`s alive!" << endl;
     Pushnoy(32, 3, 0);
     return 0;
