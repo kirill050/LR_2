d78bbf4cd06ff3a01bb96eca221f7bd485b6e83f
kirill050 <kirilltikhonov050@gmail.com>
2019-12-12 16:31:10 +0300

    12.12.19 16:31

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,15 +1,7 @@
 // Copyright 2019 Kirill <your_email>
 
 #include <gtest/gtest.h>
-#include <header.hpp>
 
 TEST(Example, EmptyTest) {
-    cache experiment;
-    experiment.start();
     EXPECT_TRUE(true);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
